6924d98485d06c163d3831a9ef3f0ae93d9003ac
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-03 22:25:04 -0600

    add error handling to get and post data

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -5,7 +5,18 @@
       <div>
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
+
+      <p v-if="isLoading">Loading...</p>
+
+      <p v-else-if="!isLoading && error">
+        {{ error }}
+      </p>
+
+      <p v-else-if="!isLoading && (!results || results.length === 0)">
+        No stored experiences found. Start adding some survey results first.
+      </p>
+
+      <ul v-else>
         <survey-result v-for="result in results" :key="result.id" :name="result.name" :rating="result.rating">
         </survey-result>
       </ul>
@@ -22,17 +33,21 @@ export default {
   },
   data() {
     return {
-      results: []
+      results: [],
+      isLoading: false,
+      error: null
     }
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch('https://vuebasics10-default-rtdb.firebaseio.com/surveys.json')
         .then((response) => {
           if (response.ok) {
             return response.json();
           }
         }).then((data) => {
+          this.isLoading = false;
           const dataArray = [];
           for (let id in data) {
             dataArray.push({
@@ -42,8 +57,17 @@ export default {
             });
           }
           this.results = dataArray;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+          this.error = "Failed to fetch data - please try again later.";
         });
+
     }
+  },
+  mounted() {
+    this.loadExperiences();
   }
 };
 </script>
